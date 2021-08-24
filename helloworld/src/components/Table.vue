414f2b42a82bba2c00933d4dfbb18592a05f38fa
wenhui <wenhui@miniasp.com>
2021-08-24 15:28:58 +0800

    改為官方範例寫法

--- helloworld/src/components/Table.vue
+++ helloworld/src/components/Table.vue
@@ -1,61 +1,30 @@
 <template>
   <div>
-    <vxe-toolbar>
-      <template #buttons>
-        <vxe-button @click="allAlign = 'left'">居左</vxe-button>
-        <vxe-button @click="allAlign = 'center'">居中</vxe-button>
-        <vxe-button @click="allAlign = 'right'">居右</vxe-button>
-      </template>
-    </vxe-toolbar>
-
-    <vxe-table :align="allAlign" :data="tableData">
-      <vxe-table-column type="seq" width="60"></vxe-table-column>
-      <vxe-table-column field="name" title="Name"></vxe-table-column>
-      <vxe-table-column field="sex" title="Sex"></vxe-table-column>
-      <vxe-table-column field="age" title="Age"></vxe-table-column>
+    <vxe-table :data="tableData">
+      <vxe-column type="seq" title="Seq" width="60"></vxe-column>
+      <vxe-column field="name" title="Name"></vxe-column>
+      <vxe-column field="role" title="Role"></vxe-column>
+      <vxe-colgroup title="Group1">
+        <vxe-column field="sex" title="Sex"></vxe-column>
+        <vxe-column field="address" title="Address"></vxe-column>
+      </vxe-colgroup>
     </vxe-table>
   </div>
 </template>
-<script>
-export default {
-  data() {
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup () {
+    const tableData = ref([
+      { id: 10001, name: 'Test1', role: 'Develop', sex: 'Man', address: 'Shenzhen' },
+      { id: 10002, name: 'Test2', role: 'Test', sex: 'Man', address: 'Guangzhou' },
+      { id: 10003, name: 'Test3', role: 'PM', sex: 'Man', address: 'Shanghai' }
+    ])
     return {
-      allAlign: null,
-      tableData: [
-        {
-          id: 10001,
-          name: "Test1",
-          role: "Develop",
-          sex: "Man",
-          age: 28,
-          address: "vxe-table 从入门到放弃",
-        },
-        {
-          id: 10002,
-          name: "Test2",
-          role: "Test",
-          sex: "Women",
-          age: 22,
-          address: "Guangzhou",
-        },
-        {
-          id: 10003,
-          name: "Test3",
-          role: "PM",
-          sex: "Man",
-          age: 32,
-          address: "Shanghai",
-        },
-        {
-          id: 10004,
-          name: "Test4",
-          role: "Designer",
-          sex: "Women",
-          age: 24,
-          address: "Shanghai",
-        },
-      ],
-    };
-  },
-};
+      tableData
+    }
+  }
+})
 </script>
