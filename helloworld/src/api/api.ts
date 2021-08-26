20ef6a4ac78431ae633539e9dbff987e535cf4e7
wenhui <wenhui@miniasp.com>
2021-08-26 15:47:40 +0800

    新增CRUD所需API

--- helloworld/src/api/api.ts
+++ helloworld/src/api/api.ts
@@ -1,7 +1,14 @@
 import axios from 'axios';
+import {DataModel} from '../model/model';
 
 const request = axios.create({
     baseURL: process.env.VUE_APP_API_URL,
 })
 
 export const getAllData = () => request.post('api/CRUD/GetAll');
+
+export const addData = (param:DataModel) => request.post('api/CRUD/Add',param);
+
+export const deleteData = (ID:string) => request.post('api/CRUD/Delete',ID);
+
+export const updateData = (param:DataModel) => request.post('api/CRUD/Update',param);
