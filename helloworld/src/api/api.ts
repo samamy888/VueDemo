ce1b7c03ca3de76b5a29ab92cbddf4e78da20e5d
wenhui <wenhui@miniasp.com>
2021-08-27 17:28:22 +0800

    新增編輯與刪除

--- helloworld/src/api/api.ts
+++ helloworld/src/api/api.ts
@@ -3,12 +3,13 @@ import {DataModel} from '../model/model';
 
 const request = axios.create({
     baseURL: process.env.VUE_APP_API_URL,
+    headers: { 'Content-Type': 'application/json' },
 })
 
 export const getAllData = () => request.post('api/CRUD/GetAll');
 
 export const addData = (param:DataModel) => request.post('api/CRUD/Add',param);
 
-export const deleteData = (ID:string) => request.post('api/CRUD/Delete',ID);
+export const deleteData = (ID:number) => request.post('api/CRUD/Delete',ID);
 
 export const updateData = (param:DataModel) => request.post('api/CRUD/Update',param);
--- helloworld/src/components/Table.vue
+++ helloworld/src/components/Table.vue
@@ -15,18 +15,50 @@
     <p>
       <vxe-table :data="tableData">
         <vxe-column field="id" title="ID"></vxe-column>
+        <vxe-column title="編輯">
+          <template #default="{ row }">
+            <vxe-button
+              content="編輯"
+              status="warning"
+              @click="OpenEditModal(row)"
+            ></vxe-button>
+          </template>
+        </vxe-column>
+        <vxe-column title="刪除">
+          <template #default="{ row }">
+            <vxe-button
+              content="刪除"
+              status="danger"
+              @click="deleteEvent(row)"
+            ></vxe-button>
+          </template>
+        </vxe-column>
         <vxe-column field="name" title="Name"></vxe-column>
       </vxe-table>
     </p>
-    <vxe-modal id="AddModal" v-model="modal.value" :title="modal.title">
+    <vxe-modal id="addModal" v-model="addModal.value" :title="addModal.title">
       <vxe-form :data="formData" @submit="addEvent()" :rules="formRule">
         <vxe-form-item title="名称" field="name">
-          <vxe-input v-model="formData.name"  placeholder="Name"></vxe-input>
+          <vxe-input v-model="formData.name" placeholder="Name"></vxe-input>
         </vxe-form-item>
         <vxe-form-item align="center" span="24">
           <vxe-button
             content="新增"
             status="success"
+            type="submit"
+          ></vxe-button>
+        </vxe-form-item>
+      </vxe-form>
+    </vxe-modal>
+    <vxe-modal id="editModal" v-model="editModal.value" :title="editModal.title">
+      <vxe-form :data="formEditData" @submit="editEvent()" :rules="formRule">
+        <vxe-form-item title="名称" field="name">
+          <vxe-input v-model="formEditData.name" placeholder="Name"></vxe-input>
+        </vxe-form-item>
+        <vxe-form-item align="center" span="24">
+          <vxe-button
+            content="編輯"
+            status="warning"
             type="submit"
           ></vxe-button>
         </vxe-form-item>
@@ -38,9 +70,9 @@
 <script lang="ts">
 import { defineComponent, onMounted, ref } from "vue";
 import { AxiosResponse } from "axios";
-import { addData, getAllData } from "../api/api";
+import { addData, deleteData, getAllData, updateData } from "../api/api";
 import { resultModel, DataModel } from "../model/model";
-import VXETable from 'vxe-table'
+import VXETable from "vxe-table";
 export default defineComponent({
   setup() {
     const tableData = ref<DataModel>();
@@ -48,9 +80,17 @@ export default defineComponent({
       id: 0,
       name: "",
     });
-    const modal = ref({
+    const formEditData = ref<DataModel>({
+      id: 0,
+      name: "",
+    });
+    const addModal = ref({
       value: false,
       title: "新增資料",
+    });
+    const editModal = ref({
+      value: false,
+      title: "編輯資料",
     });
     const formRule = ref({
       name: [{ required: true, message: "请输入名称" }],
@@ -70,19 +110,55 @@ export default defineComponent({
         });
     };
     const openModal = () => {
-      modal["value"].value = true;
+      addModal["value"].value = true;
     };
     const addEvent = () => {
-      console.log(VXETable.modal.close('AddModal'));
+      VXETable.modal.close("addModal");
       addData(formData.value as DataModel)
         .then((res: AxiosResponse<resultModel>) => {
           if (res.data.isSuccess) {
             alert("新增成功");
-            searchEvent();
-          }else{
+          } else {
             alert("新增失敗");
           }
-
+          searchEvent();
+        })
+        .catch((ex) => {
+          console.log(ex);
+        });
+    };
+    const editEvent = () => {
+      VXETable.modal.close("editModal");
+      updateData(formEditData.value as DataModel)
+        .then((res: AxiosResponse<resultModel>) => {
+          if (res.data.isSuccess) {
+            alert("編輯成功");
+          } else {
+            alert("編輯失敗");
+          }
+          searchEvent();
+        })
+        .catch((ex) => {
+          console.log(ex);
+        });
+    };
+    const OpenEditModal = (row) => {
+      editModal["value"].value = true;
+      formEditData.value = {
+        id:row.id,
+        name:row.name,
+      }
+    };
+    const deleteEvent = (row) => {
+      console.log(row.id);
+      deleteData(row.id)
+        .then((res: AxiosResponse<resultModel>) => {
+          if (res.data.isSuccess) {
+            alert("刪除成功");
+          } else {
+            alert("刪除失敗");
+          }
+          searchEvent();
         })
         .catch((ex) => {
           console.log(ex);
@@ -92,9 +168,14 @@ export default defineComponent({
       tableData,
       searchEvent,
       addEvent,
+      deleteEvent,
+      editEvent,
       openModal,
-      modal,
+      OpenEditModal,
+      addModal,
+      editModal,
       formData,
+      formEditData,
       formRule,
     };
   },
