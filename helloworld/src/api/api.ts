b943c9e8018e7495d1e82fbfe440d67ffc84f272
蕭文暉 wenhui <wenhui@miniasp.com>
2021-10-08 15:22:58 +0800

    API改成Restful格式

--- helloworld/src/api/api.ts
+++ helloworld/src/api/api.ts
@@ -11,8 +11,8 @@ const request = axios.create({
 export const jwtToken = localStorage.getItem("token");
 
 request.interceptors.request.use(
-  function(config:AxiosRequestConfig) {
-    config.headers.Authorization = 'Bearer ' + jwtToken;
+  function(config: AxiosRequestConfig) {
+    config.headers.Authorization = "Bearer " + jwtToken;
     return config;
   },
   function(error) {
@@ -22,26 +22,24 @@ request.interceptors.request.use(
   }
 );
 request.interceptors.response.use(
-  (response) => {   
+  (response) => {
     return response;
   },
   (error) => {
-    const {response} = error;
-    if (response?.status===401) {      
-      router.push('/SignIn');
+    const { response } = error;
+    if (response?.status === 401) {
+      router.push("/SignIn");
     }
     return Promise.reject(error);
   }
 );
-export const getAllData = () => request.post("api/CRUD/GetAll");
-
+export const getAllData = () => request.get("api/CRUD/GetAll");
 export const addData = (param: DataModel) =>
   request.post("api/CRUD/Add", param);
-
-export const deleteData = (ID: number) => request.post("api/CRUD/DeleteV2", ID);
-
+export const deleteData = (ID: number) =>
+  request.delete("api/CRUD/Delete", { data: ID });
 export const updateData = (param: DataModel) =>
-  request.post("api/CRUD/UpdateV2", param);
+  request.put("api/CRUD/Update", param);
 
 export const signIn = (login: LoginModel) =>
   request.post("api/Auth/SignIn", login);
